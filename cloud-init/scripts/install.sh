ce8b9c75a20e559ad66723e68468d0aeb1cd5b45
conor-mccullough <conor.mccullough@hashicorp.com>
2023-03-29 16:57:32 +1100

    More commits

--- cloud-init/scripts/install.sh
+++ cloud-init/scripts/install.sh
@@ -3,8 +3,6 @@
 # Remember - IP_ADDR=$(curl ifconfig.me)
 IP_ADDR=$(ip a | grep enp0s1 | awk -v RS='([0-9]+\\.){3}[0-9]+' 'RT{print RT}' | head -n 1)
 HOSTNAME=$(hostname)
-# Take off here from the k3s psql install script
-./postgres-install.sh
 
 sudo openssl req -new -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out boundarycert.crt -keyout boundarycert.key -subj '/C=AU/ST=Melbourne/L=Melbourne/O=Hashicorp/OU=Vault Support Engineering/CN=localhost' -addext "subjectAltName = DNS:${HOSTNAME}"
 
@@ -107,9 +105,12 @@ kms "aead" {
 
 EOF
 
+# Take off here from the k3s psql install script
+./postgres-install.sh
+
 sudo mkdir -p /opt/boundary/certs
 sudo chown boundary:boundary /usr/local/bin/boundary
-sudo cp /home/ubuntu/license.hclic /opt/boundary/ #&& sudo cp boundary*hcl /opt/boundary
+sudo cp /home/ubuntu/license.hclic /opt/boundary/ 
 sudo cp boundarycert.* /opt/boundary/certs
 sudo chown -R boundary:boundary /opt/boundary
 
@@ -125,8 +126,5 @@ sudo systemctl enable boundary.service
 sudo systemctl start boundary.service
 sudo systemctl enable boundary-worker.service
 sudo systemctl start boundary-worker.service
-```
-'
 
-sudo echo 'he'
 echo "Remember to export BOUNDARY_ADDR=https://127.0.0.1:9200, etc"
